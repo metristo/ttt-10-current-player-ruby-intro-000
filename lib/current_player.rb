6f4770a21e8b212355763a0432be40e02aa9b3be
Michael G Rivera <rivera.mgr@gmail.com>
2020-05-20 19:13:45 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,10 @@ def turn_count(board)
   counter = 0
   turns = board.size
   turns.each do |token|
-    counter += 1
-
+    if {board} == "X" || "O"
+      counter += 1
+    else
+      counter +=0
+end
   end
 end
